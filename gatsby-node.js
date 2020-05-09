aae4310678df0671263c77cb331bb44d54590abc
Cory Reed <swashcap@gmail.com>
2020-05-09 16:31:49 -0700

    fix: re-add slug to .mdx nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,18 +3,19 @@ const {createFilePath} = require('gatsby-source-filesystem');
 /**
  * Create slugs for `news` nodes
  * {@link https://www.gatsbyjs.org/docs/creating-slugs-for-pages/}
+ * {@link https://www.gatsbyjs.org/docs/mdx/programmatically-creating-pages/}
  */
 exports.onCreateNode = ({actions: {createNodeField}, getNode, node}) => {
-  // if (node.internal.type === 'Mdx') {
-  //   const value = createFilePath({
-  //     basePath: 'src/news',
-  //     getNode,
-  //     node,
-  //   });
-  //   createNodeField({
-  //     name: 'slug',
-  //     node,
-  //     value,
-  //   });
-  // }
+  if (node.internal.type === 'Mdx') {
+    const value = createFilePath({
+      basePath: 'src/news',
+      getNode,
+      node,
+    });
+    createNodeField({
+      name: 'slug',
+      node,
+      value,
+    });
+  }
 };
